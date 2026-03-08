2f0e681b431bc4bc84aa22c3dfed8060c05202fa
Gove <3425519400@qq.com>
2026-03-08 21:59:51 +0800

    测试一下能不能用

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("测试一下能不能用");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
