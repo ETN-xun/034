d1e56e5ad563d0438de83a4bcd67ba0f0638392c
Gove <3425519400@qq.com>
2026-03-10 13:10:33 +0800

    尝试重构失败！

---
+++ Assets/Scripts/Config/ElectronicConfig.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public class ElectronicConfig
+{
+    public string Name = "电子元器件";
+    public Color Color = Color.white;
+    public ShapeType Shape = ShapeType.None;
+    public RuleType Rule = RuleType.None;
+}
---
+++ Assets/Scripts/Electronic/ShapeType.cs
@@ -0,0 +1,10 @@
+
+
+public enum ShapeType
+{
+    None,
+    Sine,
+    Square,
+    Triangle,
+    // 可以根据需要添加更多的波形类型
+}
